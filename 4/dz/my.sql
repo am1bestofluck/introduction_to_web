7df7bb8ad1e382c70f3b6c70cb337aedb370a667
am1bestofluck <108458610+am1bestofluck@users.noreply.github.com>
2023-01-30 19:54:59 +0200

    Update my.sql

--- 4/dz/my.sql
+++ 4/dz/my.sql
@@ -5,13 +5,13 @@ CREATE TABLE COLLEAGUES (
   age INTEGER,
   address TEXT);
 
-INSERT INTO COLLEAGUES(name, age, address) VALUES ("Louie Fox",22, "London, bd Victory 1");
+INSERT INTO COLLEAGUES(name, age, address) VALUES ("Louie Fox",22, "London, bd Mercy 1");
 INSERT INTO COLLEAGUES (name, age, address) VALUES ('Philip King',50,'Moscow, bd Peace 3');
-INSERT INTO COLLEAGUES (name, age, address) VALUES ('Ivan Hussain',24,'Moscow, bd Victory 5');
-INSERT INTO COLLEAGUES (name, age, address) VALUES ('Lochlan Woods',25,'Canberra, bd Victory 7');
-INSERT INTO COLLEAGUES (name, age, address) VALUES ('Reggie Barker',26,'Texas, bd Victory 9');
-INSERT INTO COLLEAGUES (name, age, address)  VALUES ('Aaryan Smith',22,'Kiev, bd Victory 11');
-INSERT INTO COLLEAGUES (name, age, address)VALUES  ('Callum Wright',27,'Chisinau, bd Victory 15');
+INSERT INTO COLLEAGUES (name, age, address) VALUES ('Ivan Hussain',24,'Moscow, bd Wheat 5');
+INSERT INTO COLLEAGUES (name, age, address) VALUES ('Lochlan Woods',25,'Canberra, bd Song 7');
+INSERT INTO COLLEAGUES (name, age, address) VALUES ('Reggie Barker',26,'Texas, bd Smile 9');
+INSERT INTO COLLEAGUES (name, age, address)  VALUES ('Aaryan Smith',22,'Kiev, bd Garden 11');
+INSERT INTO COLLEAGUES (name, age, address)VALUES  ('Callum Wright',27,'Chisinau, bd Bride 15');
 
 SELECT name FROM COLLEAGUES WHERE address LIKE 'Moscow%' and age > 19 and age < 30
 
